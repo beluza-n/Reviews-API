cab8f8143dd06217ef32275cd31e7bbc264d1890
Gatart <58820623+gatart@users.noreply.github.com>
2023-12-06 14:41:55 +0300

    Beluza added role and bio to csv_import (#9)
    * Models of Title, Cagetory, Genre
    
    * Checked max_lengts. Removed GenreTitle
    
    * Вся моя часть в рабочем состоянии
    
    * Все мои изменения для pull request'a.
    
    * Fixed imports
    
    * Первая рабочая версия csv_import
    
    * Создание через save заменено на bulk_create
    
    * Добавлена возможность указывать путь через --path
    
    * Add role and bio in csv_import of user model
    
    ---------
    
    Co-authored-by: Анастасия Гречкина <beluza@mail.ru>

--- api_yamdb/csv_import/management/commands/csv_import.py
+++ api_yamdb/csv_import/management/commands/csv_import.py
@@ -114,6 +114,8 @@ class Command(BaseCommand):
             user=User(id=row['id'],
                         username=row['username'],
                         email=row['email'],
+                        role=row['role'],
+                        bio=row['bio'],
                         first_name=row['first_name'],
                         last_name=row['last_name'])  
             bulk_list.append(user)
